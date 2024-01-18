ac023ddd7a706c9dd6924dd81c36a7c17af26e74
sonu2614 <60311092+sonu2614@users.noreply.github.com>
2024-01-18 12:26:15 +0530

    Initial commit

---
+++ models/todos/todo.models.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose"
+
+const todoSchema = new mongoose.Schema(
+  {
+    content:{
+      type:String,
+      reuired:true,
+    },
+    
+    complete:{
+      type : Boolean,
+      default : false
+    },
+
+    createdBy:{
+      type:mongoose.Schema.Types.ObjectId,
+      ref:"User"
+    },
+
+    subTodos:[
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref:"SubTodo"
+      }
+    ]// Array of Sub-Todos
+
+  },{timestamps:true});
+
+  export const Todo=mongoose.model("Todo",todoSchema)
